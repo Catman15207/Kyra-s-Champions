a37f5d373ebfe0d0f102d1094630bed7a0832232
Catman15207 <jromero4@lions.molloy.edu>
2025-10-09 11:35:33 -0400

    Add files

---
+++ src/pages/Resume.js
@@ -0,0 +1,50 @@
+import ResumeItem from "../components/ResumeItem";
+
+export default function Resume() {
+  return (
+    <>
+      <h1>Alvaro Montoro</h1> 
+      
+       <p>
+       Senior Software Engineer with strong experience creating
+       high-quality, scalable, and high-performance web applications seeking
+       front-end and full stack opportunities.
+       </p> 
+
+      <h2>EXPERIENCE FILTERS</h2>
+
+      <h3>athenahealth Healthcare IT</h3>
+
+      <h4> Member of Technical Staff  <time> 2017 - 2019 </time> </h4> 
+      <li>
+      Developed widgetsusing React and Less to display patient information.
+      </li>
+
+      <li>
+      Created design specifications, participated in design and code reviews.
+      </li> Worked on the
+      design and initial development of a new platform to import medical
+      documents. Developed a Kotlin micro-service to generate XML documents
+      based on a government-compliant architecture (CCDA). General Motors
+      Cadillac.Com Team Web Developer 2015 - 2017 Developed new components for
+      the award-winning website Cadillac.com and its mobile version. Worked with
+      stakeholders to refine and negotiate product requirement specifications,
+      functional specifications, and product vision and direction. Designed a
+      web component that reduced page load speed by over 15%, improving SEO and
+      accessibility, and saving authoring expenses. Organized and participated
+      in events representing GM at volunteering opportunities and competitions.
+      MicroAssist Information Technology and Services PHP Developer 2014 - 2015
+      Designed and developed different web projects with a focus on
+      accessibility, including new features and website enhancements. Created
+      and integrated an automated mail system with 3rd party APIs. Developed and
+      collaborated in the translation project of the ITTD website for the Texas
+      Department of Public Safety. Hewlett-Packard Content & Digital Asset
+      Management IT Engineer 2011 - 2014 Developed two internal content
+      management systems (related to HP’s products documentation) with over
+      three million monthly visitors. Coordinated efforts with an overseas team,
+      applying Agile methodologies to ensure on-time, on-spec project delivery.
+      Improved different batch jobs performance, reducing execution time by over
+      50%, and eliminating downtime.
+    </>
+  );
+}
