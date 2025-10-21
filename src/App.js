5998fad1e26f04d3a4d3dee74a45c0ccbb35d5e8
catman15207 <jromero4@lions.molloy.edu>
2025-10-21 02:41:45 +0000

    Add routing with Resume, About, Contact pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import "./styles.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import Resume from "./pages/Resume";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 export default function App() {
-  return <Resume />;
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Resume />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
